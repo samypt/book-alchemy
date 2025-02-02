17dd492da2c463cb0c6358d1a0b5ede919fc1fb9
samypt <vitaliiptichkin@gmail.com>
2025-02-02 17:08:51 +0100

    added seach and sort functionality and css styles

--- app.py
+++ app.py
@@ -20,17 +20,50 @@ if not os.path.exists(DB_PATH):
         print("New DB Created")
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'] )
 def home():
-    return render_template('home.html')
+    choice = 'title'
+    if request.method == "POST":
+        try:
+            choice = request.form.get('sorting')
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+    return render_template('home.html', books=get_books_from_db(choice), order=choice)
+
+
+@app.route('/search', methods=['POST'])
+def search_book():
+    if request.method == "POST":
+        try:
+            search_key = request.form.get('key')
+            if not search_key.strip():
+                return redirect('/')
+            return render_template('home.html', books=get_books_by_title(search_key))
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+    return None
 
 
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
     if request.method == 'POST':
         new_author = add_author_to_db()
-        return render_template("notification.html", obj=new_author)
+        return render_template("notification.html",
+                               obj=new_author, obj_type=type(new_author).__name__)
     return render_template('add_author.html')
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        new_book = add_book_to_db()
+        return render_template("notification.html",
+                               obj=new_book, obj_type=type(new_book).__name__)
+
+    authors = get_authors_from_db()
+    return render_template('add_book.html', authors = authors)
 
 
 def add_author_to_db():
@@ -54,9 +87,9 @@ def add_author_to_db():
 
         # Create and add the new author
         new_author = Author(
-            author_name=author_name,
-            author_birth_date=birth_date,
-            author_date_of_death=death_date,
+            name=author_name,
+            birth_date=birth_date,
+            date_of_death=death_date,
         )
         db.session.add(new_author)
         db.session.commit()
@@ -65,6 +98,60 @@ def add_author_to_db():
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+
+def add_book_to_db():
+    book_isbn = request.form.get('isbn')
+    book_title = request.form.get('title')
+    book_publication_year = request.form.get('publication_year')
+    author_id = request.form.get('author')
+
+    if not (book_title or author_id):
+        return None
+
+    try:
+        publication_year = (
+            datetime.strptime(book_publication_year, "%Y-%m-%d").date()
+            if book_publication_year else None
+        )
+        new_book = Book(
+            isbn=book_isbn,
+            title=book_title,
+            publication_year=publication_year,
+            author_id=author_id)
+        db.session.add(new_book)
+        db.session.commit()
+        print('A new book has been successfully added to the database')
+        return new_book
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+def get_authors_from_db(order='name'):
+    authors = Author.query.order_by(order)
+    return authors
+
+
+def get_books_from_db(order='title'):
+    if order == 'author':
+        books = Book.query.join(Author).order_by(Author.name).all()
+    else:
+        books = Book.query.order_by(getattr(Book, order)).all()
+    return books
+
+
+def get_books_by_title(title):
+    if title:
+        try:
+            books = Book.query.filter(Book.title.like(f"%{title}%")).all()
+            print(books)
+            return books
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+
 
 
 if __name__ == '__main__':
--- data_models.py
+++ data_models.py
@@ -6,34 +6,38 @@ db = SQLAlchemy()
 class Author(db.Model):
     __tablename__ = 'authors'
 
-    author_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    author_name = db.Column(db.String, nullable=False)
-    author_birth_date = db.Column(db.Date, nullable=True)
-    author_date_of_death = db.Column(db.Date, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
+    date_of_death = db.Column(db.Date, nullable=True)
     # Relationship
     books = db.relationship('Book', backref='author', lazy=True)
 
     def __str__(self):
-        return  f"Author: {self.author_name} with id : {self.author_id} has been successfully added to the database"
+        return  f"Author: {self.name} with id : {self.id} has been successfully added to the database"
 
     def __repr__(self):
-        return (f"Author (id = {self.author_id}, name = {self.author_name}, "
-                f"bd = {self.author_birth_date}, dd = {self.author_date_of_death})")
+        return (f"Author (id = {self.id}, name = {self.author_name}, "
+                f"bd = {self.birth_date}, dd = {self.date_of_death})")
 
 
 class Book(db.Model):
     __tablename__ = 'books'
 
-    book_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    book_isbn = db.Column(db.String, nullable=False)
-    book_title = db.Column(db.String, nullable=False)
-    book_publication_year = db.Column(db.Date, nullable=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    publication_year = db.Column(db.Date, nullable=True)
     # Foreign Key to Author
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
     # Relationship
     # author = db.relationship('Author', backref='books')
 
+    def __str__(self):
+        return  f"Book: {self.title} with id : {self.id} has been successfully added to the database"
+
+
     def __repr__(self):
-        return (f"Book (id = {self.book_id}, title = {self.book_title}, "
-                f"isbn = {self.book_isbn}, pby = {self.book_publication_year}, "
+        return (f"Book (id = {self.id}, title = {self.title}, "
+                f"isbn = {self.isbn}, pby = {self.publication_year}, "
                 f"aut_id = {self.author_id}, author = {self.author})")
